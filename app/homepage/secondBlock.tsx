a18f27a97ec33aef9237b03258ecf8cbacfc7067
Ariyaan <134138177+Ariyaan321@users.noreply.github.com>
2024-05-17 20:47:46 +0530

    second block 90%

--- app/homepage/secondBlock.tsx
+++ app/homepage/secondBlock.tsx
@@ -1,62 +1,46 @@
 import React from 'react'
 import Image from 'next/image'
-import First from './images/first.webp'
+import Second from './images/second.webp'
 
 const secondBlock = () => {
     return (
         <>
 
             {/* Second block */}
-            <div className='w-full h-[680px] bg-white border-solid border-2 border-red-500 px-[64px] py-[64px] content-center flex justify-center'>
-                <div className='border-solid border-2  flex items-center border-sky-500 justify-between w-[1140px] h-[555px]'>
-                    <div className='w-[454px] h-[383px] border-solid border-2 border-green-600'> {/* Left side */}
-                        <div className='h-[191px] border-solid border-1 border-yellow-300'> {/* Upper part aka 1st half */}
-                            <div className='h-[191px] border-solid border-2 border-yellow-300 flex flex-col justify-center'>
-                                <h1 className='text-[52px] font-bold leading-tight text-white'>Go anywhere with Uber</h1>
-                                <p className='content-end text-[16px] text-white font-normal'>Request a ride, hop in, and go.</p>
+            <div className='w-full h-[680px] bg-white border-solid border-2 border-red-900 px-[64px] py-[64px] content-center flex justify-center'>
+                <div className='border-solid border-2  flex flex-row-reverse items-center border-sky-500 justify-between w-[1140px] h-[555px]'>
+                    <div className='w-[454px] h-[383px] border-solid border-2 border-green-600 flex-col flex justify-between'> {/* right side */}
+                        <div className='border-solid border-1 border-yellow-300'> {/* Upper part aka 1st half */}
+                            <div className='h-[191px] border-solid border-2 border-yellow-300'>
+                                <h1 className='text-[52px] font-bold leading-tight text-black border-solid border-4 border-red-900'>Drive when you want, make what you need</h1>
+                                <p className='h-max text-[16px] text-black font-normal leading-6 mt-6 border-solid border-4 border-green-900'>Make money on your schedule with deliveries or rides—or both. You can use your own car or choose a rental through Uber.</p>
                             </div>
-                        </div>
-                        <div className='h-[191px] border-solid border-2 border-red-400 mt-2'>  {/* Down Part aka 2nd half */}
+                        </div> {/* 1st upper half end */}
 
-                            <div className='w-[396px] h-[47px] bg-white rounded-lg flex border-solid border-4 border-red-600'> {/* Enter location : hold 3 elems -> 1svg , 1 i/p , 1svg */}
-                                <div className='ml-3'>
-                                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" data-baseweb="icon"><title>search</title><path fill-rule="evenodd" clip-rule="evenodd" d="M12 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm5-2a5 5 0 1 1-10 0 5 5 0 0 1 10 0Z" fill="currentColor"></path></svg>
-                                </div>
-
-                                <div className='ml-1.2 border-solid border-4 border-yellow-600 w-full'>
-                                    <input className='w-full text-[17px]' type="text" placeholder='Enter location' />
-                                </div>
-
-                                <div className='ml-auto mr-3'>
-                                    <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" role="button"><title>Navigate right up</title><path d="M10.5 13.5.5 11 21 3l-8 20.5-2.5-10Z" fill="currentColor"></path></svg>
-                                </div>
+                        {/* 2nd half : button and Sign in */}
+                        <div className='flex gap-x-7 border-solid border-2 border-red-900 h-fit items-center'>
+                            <div className='w-[135px] h-[48px] bg-black rounded-md flex justify-center items-center hover:bg-gray-900 duration-200'>
+                                <a href="/" className='text-white h-fit border-solid border-2 border-red-900'>Get started</a>
                             </div>
 
-                            <div className='absolute bg-white h-[20px] w-[1px] ml-6 ' />
+                            <div className='items-end h-fit'>
+                                <span className='text-black'>
+                                    Already have an account? Sign in
+                                    {/* <div className='w-30 h-7 bg-black transform scale-x-0 transition-transform duration-300 ease-in-out hover:scale-x-100'></div> */}
+                                    <div className='w-30 h-1 bg-gray-200 rounded-3xl'></div>
+                                    <div className='w-30 h-1 rounded-3xl hover:{before:bg-grey-200 after:bg-black}'></div>
 
-                            {/*2nd input */}
-                            <div className='w-[396px] h-[47px] mt-[12px] bg-white rounded-lg flex border-solid border-4 border-red-600'> {/* Enter destination : hold 2 elems -> 1svg , 1 i/p*/}
-                                <div className='ml-3'>
-                                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" data-baseweb="icon"><title>search</title><path fill-rule="evenodd" clip-rule="evenodd" d="M14 10h-4v4h4v-4ZM7 7v10h10V7H7Z" fill="currentColor"></path></svg>
-                                </div>
-
-                                <div className='ml-1.2 border-solid border-4 border-yellow-600 w-full'>
-                                    <input className='w-full text-[17px]' type="text" placeholder='Enter destination' />
-                                </div>
+                                </span>
                             </div>
+                        </div> {/*2nd half end*/}
 
-                            {/*Button ( Link! <a />)- see prices */}
-                            <div className='w-[127px] h-[48px] mt-[16px] rounded-lg bg-white flex justify-center'>
-                                <a href="/" className='text-black  text-[16px] font-medium content-center'>See prices</a>
-                            </div>
 
-                        </div>
                     </div>
 
                     {/* Right Side : Image */}
                     <div className=''>
                         <Image
-                            src={First}
+                            src={Second}
                             width={552}
                             height={552}
                             alt="Picture of the author"

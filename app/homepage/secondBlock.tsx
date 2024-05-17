a501f3508b141c44f4ae0163efba6fa6b5ca4501
Ariyaan <134138177+Ariyaan321@users.noreply.github.com>
2024-05-17 21:38:42 +0530

    second block done

--- app/homepage/secondBlock.tsx
+++ app/homepage/secondBlock.tsx
@@ -7,26 +7,25 @@ const secondBlock = () => {
         <>
 
             {/* Second block */}
-            <div className='w-full h-[680px] bg-white border-solid border-2 border-red-900 px-[64px] py-[64px] content-center flex justify-center'>
-                <div className='border-solid border-2  flex flex-row-reverse items-center border-sky-500 justify-between w-[1140px] h-[555px]'>
-                    <div className='w-[454px] h-[383px] border-solid border-2 border-green-600 flex-col flex justify-between'> {/* right side */}
-                        <div className='border-solid border-1 border-yellow-300'> {/* Upper part aka 1st half */}
-                            <div className='h-[191px] border-solid border-2 border-yellow-300'>
-                                <h1 className='text-[52px] font-bold leading-tight text-black border-solid border-4 border-red-900'>Drive when you want, make what you need</h1>
-                                <p className='h-max text-[16px] text-black font-normal leading-6 mt-6 border-solid border-4 border-green-900'>Make money on your schedule with deliveries or rides—or both. You can use your own car or choose a rental through Uber.</p>
+            <div className='w-full h-[680px] bg-white border-solid border-2 border-purple-900 px-[64px] py-[64px] content-center flex justify-center'>
+                <div className='flex flex-row-reverse items-center justify-between w-[1140px] h-[555px]'>
+                    <div className='w-[454px] h-[383px] flex-col flex justify-between'> {/* right side */}
+                        <div className='border-solid border-3 border-yellow-300'> {/* Upper part aka 1st half */}
+                            <div className='h-[191px]'>
+                                <h1 className='text-[52px] font-bold leading-tight text-black'>Drive when you want, make what you need</h1>
+                                <p className='h-max text-[16px] text-black font-normal leading-6 mt-8'>Make money on your schedule with deliveries or rides—or<br /> both. You can use your own car or choose a rental <br /> through Uber.</p>
                             </div>
                         </div> {/* 1st upper half end */}
 
                         {/* 2nd half : button and Sign in */}
-                        <div className='flex gap-x-7 border-solid border-2 border-red-900 h-fit items-center'>
-                            <div className='w-[135px] h-[48px] bg-black rounded-md flex justify-center items-center hover:bg-gray-900 duration-200'>
-                                <a href="/" className='text-white h-fit border-solid border-2 border-red-900'>Get started</a>
+                        <div className='flex gap-x-7 h-fit items-center'>
+                            <div className='w-[135px] h-[48px] bg-black rounded-md flex justify-center items-center hover:bg-GG duration-200'>
+                                <a href="/" className='text-white h-fit'>Get started</a>
                             </div>
 
                             <div className='items-end h-fit'>
-                                <span className='text-black'>
+                                <span className='text-black cursor-pointer'>
                                     Already have an account? Sign in
-                                    {/* <div className='w-30 h-7 bg-black transform scale-x-0 transition-transform duration-300 ease-in-out hover:scale-x-100'></div> */}
                                     <div className='w-30 h-1 bg-gray-200 rounded-3xl'></div>
                                     <div className='w-30 h-1 rounded-3xl hover:{before:bg-grey-200 after:bg-black}'></div>
 
